b190f57dcb5c06b6c60a142543b15d38b1a0a870
willian <viniciussousaitb35@gmail.com>
2026-08-23 08:52:03 -0300

    adicionado springSecurity, SpringData e iniciando arquitetura de camadas

---
+++ src/main/java/dev/java/GestaoDePedido/Entity/UsuarioEntity.java
@@ -0,0 +1,27 @@
+package dev.java.GestaoDePedido.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome", length = 100)
+    private String nome;
+    @Column(name = "email", length = 100)
+    private String email;
+    @Column(name = "senha")
+    private String senha;
+
+}
