9f97c9545df4c95c6bb082134ceb314950be22a0
willian <viniciussousaitb35@gmail.com>
2026-08-23 08:08:30 -0300

    first commit

---
+++ src/main/java/dev/java/GestaoDePedido/GestaoDePedidoApplication.java
@@ -0,0 +1,14 @@
+package dev.java.GestaoDePedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+GestaoDePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDePedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/GestaoDePedido/GestaoDePedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.GestaoDePedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDePedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
